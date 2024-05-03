640118fa08e626d9cd735797ee52b25cc7ce7637
ViperSniper0501 <michaelabrenner@gmail.com>
2024-05-03 17:00:37 -0400

    updated repo to use symlinks instead of just pulling to original location and having multiple copies. need to work on changing the .zshrc to reflect this

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+home_dir="$HOME"
+
+files=$(find . -type f \( ! -name "Readme.adoc" -a ! -name "setup.sh" -a ! -path "./.git/*" -a ! -name ".gitignore" \)  | sed 's|^\./||')
+
+for file in $files; do
+
+    target_path="$home_dir/$file"
+
+    ln -f "$PWD/$file" "$target_path"
+
+    echo "Created hard symlink '$target_path' pointing to '$PWD/$file'"
+done
+
+echo "Dotfile setup complete."
