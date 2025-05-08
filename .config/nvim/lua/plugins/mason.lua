8067b25593e488458b41449b34e0d2eab970ed99
vipersniper0501 <michael@michaelbrenner.dev>
2025-05-08 14:01:59 -0400

    made it so that roslyn LSP for c# still works

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,9 +1,8 @@
-local installed_lsps = {
+local general_installed_lsps = {
     "lua_ls", "rust_analyzer", "clangd",
     "pyright", "gopls", "ts_ls", "zls", "vimls", "volar",
-    "glsl_analyzer", "rnix", "jdtls", "stylua", "checkmake",
-    "ansible-lint", "cmakelint", "roslyn", "bashls",
-    "ansible-language-server", "r-languageserver", "svelte-language-server"
+    "glsl_analyzer", "rnix", "jdtls",
+    "omnisharp", "bashls", "ansiblels", "r_language_server", "svelte"
 }
 
 local is_nvim_011 = vim.fn.has('nvim-0.11') == 1
@@ -14,6 +13,9 @@ return {
         tag = not is_nvim_011 and "v1.11.0" or nil,
         config = function()
             require("mason").setup({
+                ui = {
+                    border = "rounded"
+                },
                 registries = {
                     "github:mason-org/mason-registry",
                     "github:Crashdummyy/mason-registry",
@@ -29,7 +31,7 @@ return {
         config = function()
             if is_nvim_011 then
                 require("mason-lspconfig").setup({
-                    ensure_installed = installed_lsps,
+                    ensure_installed = general_installed_lsps,
                     automatic_enable = true,
                 })
             else
@@ -38,11 +40,14 @@ return {
         end
     },
     {
+        -- used for installing non standard mason registry LSPs (e.g. linters, LSPs from alternate registries)
         "WhoIsSethDaniel/mason-tool-installer.nvim",
-        enabled = not is_nvim_011,
+        --enabled = not is_nvim_011,
         config = function()
             require("mason-tool-installer").setup({
-                ensure_installed = installed_lsps,
+                ensure_installed = {
+                "roslyn"
+                    },
                 run_on_start = true,
             })
         end
